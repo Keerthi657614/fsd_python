2c74e2011136368e2091e4594069465cfa050e0d
Keerthi657614 <147122327+Keerthi657614@users.noreply.github.com>
2025-09-27 09:45:08 +0530

    Update settings.py

--- e_commerce/e_commerce/settings.py
+++ e_commerce/e_commerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#ycx#-av_9io#62h8kjpbbac8%t%k_sllt8xx2p=u0nnmne&ot"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
